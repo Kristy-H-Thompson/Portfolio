b0ea1bbcf9e743b70267bf619a30cb5c6aa0071b
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2022-12-02 19:16:38 -0500

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,23 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbz7ltm6HIUqw_VDom5C3NuLxLBOxuExpp5cJ5hSAGURR5We3AGN6Sv_NP0biDyshqdU/exec";
 
+const form = document.forms["oFormObject"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const bt = callme();
+  fetch(scriptURL, {
+    method: "POST",
+    body: new FormData(form),
+    mode: "no-cors"
+  })
+    .then((response) =>
+      alert(
+        "Your message has been submitted! Thank you :D" +
+          bt +
+          " ^^",
+        response
+      )
+    )
+    .catch((error) => alert("Error!", error.message));
+});
