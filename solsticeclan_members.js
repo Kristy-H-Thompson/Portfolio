114c6b3820a6b773a4f71a33ae1a314d295d00a0
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2022-12-07 12:07:48 -0500

    Update solsticeclan_members.js

--- solsticeclan_members.js
+++ solsticeclan_members.js
@@ -650,20 +650,6 @@ function callme() {
 
   return bloodtype;
 }
-
-
-
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
-}
   
 
 //Page Tabs
